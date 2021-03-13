0cde9778c0ee720023ecaad394af11df39394a74
chunyou0408 <app520321@gmail.com>
2021-03-13 13:19:56 +0800

    0313

--- resources/views/front/01-layout/09-work/index.blade.php
+++ resources/views/front/01-layout/09-work/index.blade.php
@@ -5,8 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>個人網站</title>
+
+    <!-- bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/01-layout/09-work/style.css">
+
+    <!-- aos CDN -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <!-- fontawesome CDN -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body onload="opening()">
@@ -15,17 +25,17 @@
         <header id="header" class="">
             <div class="container">
                 <div class="row">
-                    <div class="col-4">
+                    <div class="col-6 col-md-4">
                         <div class="logo_area">
                             <a href="index.html">許淳祐個人網站</a>
                         </div>
                     </div>
 
-                    <div class="col-8 menu_wrap">
+                    <div class="col-6 col-md-8 menu_wrap">
                         <nav id="navigation" class="navbar-right">
                             <ul id="nav" class="nav navbar-nav">
                                 <li><a href="index.html" style="--i:1;">首頁</a></li>
-                                <li class="current"><a href="#service_area" style="--i:2;">服務</a></li>
+                                <li class="current"><a href="#skill_area" style="--i:2;">技能</a></li>
                                 <li class=""><a href="#work_area" style="--i:3;">作品集</a></li>
                                 <li class=""><a href="#resume_area" style="--i:4;">履歷</a></li>
                                 <li><a href="#testimonial_area" style="--i:5;">見證者</a></li>
@@ -42,27 +52,40 @@
 
         <section id="home_banner_area">
             <div class="container">
-                <div class="row">
-                    <div class="col-12 text">
-                        <h1>哈囉~我是許淳祐</h1>
-                        <h1 class="animationText">一名前端，後端網頁工程師</h1>
+                <div class="row" style="height: 610px;">
+                    <div class="col-md-7 col-sm-12 col-xs-12 text">
+                        <div class="slider_content">
+                            <h1>哈囉~我是許淳祐</h1>
+                            <h1 class="animationText">一名前端，後端網頁工程師</h1>
+                        </div>
+                    </div>
+                    <div class="col-md-5 d-none d-lg-block">
+                        <div class="imgbox">
+                            <img src="/img/PLOY-210312.jpg" alt="">
+                        </div>
                     </div>
                 </div>
+            </div>
         </section>
 
         <section id="about_me_area">
             <div class="container">
                 <div class="row">
-                    <div class="col-6 photoimg">
+                    <div class="col-12 col-md-5 photoimg">
+                        <img src="/img/2020-11-06-Pen Tool_210312.png" alt="">
                     </div>
-                    <div class="col-6 text">
+                    <div class="col-12 col-md-7 text">
                         <h1>關於我</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quidem nulla eum tenetur
-                            omnis
-                            doloribus repudiandae, aspernatur rem dolores vel ex commodi voluptas reprehenderit totam
-                            beatae! Officia earum corporis inventore?</p>
+                        <p>修平科技大學資網系畢業，
+                            具備前端Html、Css(Scss)、JavaScript與後端Laravel 框架能力，
+                            喜歡思考、分析問題、解決問題，並思考有效率的解決方法。
+                            勇於學習各項的人事物，努力利用自己所學達成更好的目標，表現出自己最好的一面，保持良好的學習態度。</p>
                     </div>
                 </div>
+        </section>
+
+        <section id="skill_area">
+            <div class="container">
                 <div class="row">
                     <div class="title col-12" style="text-align: center;">
                         <h1>技能</h1>
@@ -104,72 +127,289 @@
         </section>
 
 
-        <section>
+        <section id="work_area">
 
             <div class="container">
                 <div class="row">
+                    <div class="title col-12" style="text-align: center;">
+                        <h1>作品集</h1>
+                        <p>自己的學習過程<br>
+                            製作的一個作品集</p>
+                    </div>
+                </div>
+                <!-- 選擇器 -->
+                <div class="row text-center">
+                    <div class="portfolio_filter">
+                        <ul>
+                            <li class="active" data-type="all">全部</li>
+                            <li data-type="layout">切版相關</li>
+                            <li data-type="JavaScript">JavaScript相關</li>
+                            <li data-type="work">實作相關</li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- 顯示卡片 -->
+                <div class="row">
+                    <div class="card col-12 col-md-6 col-lg-4 active layout">
+                        <img src="/img/01-layout/09-work/01.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">微軟切版</h5>
+                            <p class="card-text">課後練習題目</p>
+                        </div>
+                    </div>
+                    <div class="card col-12 col-md-6 col-lg-4 active layout">
+                        <img src="/img/01-layout/09-work/02.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">首頁切版</h5>
+                            <p class="card-text">點選圖片，會將圖片展開</p>
+                        </div>
+                    </div>
+                    <div class="card col-12 col-md-6 col-lg-4 active JavaScript">
+                        <img src="/img/01-layout/09-work/03.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">天氣卡片API</h5>
+                            <p class="card-text">課後練習題目,利用中央氣象局提供的資訊,顯示到畫面上
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card col-12 col-md-6 col-lg-4 active JavaScript">
+                        <img src="/img/01-layout/09-work/04.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">家具存貨系統-filter</h5>
+                            <p class="card-text">課後練習題目,利用伺服器回傳的資料,顯示到畫面上</p>
+                        </div>
+                    </div>
+                    <div class="card col-12 col-md-6 col-lg-4 active work">
+                        <img src="/img/01-layout/09-work/05.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">常見問題</h5>
+                            <p class="card-text">到工作室的練習切版</p>
+                        </div>
+                    </div>
+                    <div class="card col-12 col-md-6 col-lg-4 active work">
+                        <img src="/img/01-layout/09-work/06.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">聯絡我們</h5>
+                            <p class="card-text">到工作室的練習切版</p>
+                        </div>
+                    </div>
+
+
 
                 </div>
+                <!-- 查看更多作品集 -->
+                <div class="row" style="margin-top: 30px;">
+                    <button class="btn btn-primary offset-4 col-4">查看更多</button>
+                </div>
             </div>
-
-
         </section>
-    </div>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js">
-    <script>
-        // 讀取畫面
-        var myVar;
 
-        function opening() {
-            myVar = setTimeout(showPage, 3000);
-        }
+        <section id="resume_area">
+            <div class="container">
+                <div class="row">
+                    <div class="title col-12" style="text-align: center;">
+                        <h1>履歷</h1>
+                        <p>我的學習路程<br>
+                            工作上的經驗與訓練</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4 class="resume_title"><i class="fas fa-book"></i>教育</h4>
+                        <div class="single_resume" data-aos="fade-right">
+                            <span class="resume_duration">2009年9月-2013年7月</span>
+                            <h3>資訊網路技術系</h3>
+                            <span class="resume-designation">修平科技大學</span>
+                            <p>
+                                就學期間，學校訓練我具備各項程式設計能力有，有C語言、JAVA、android、資料庫等，還有一些網路管理的課程CISCO，也在畢業前成功考取了國際證照CISCO
+                                CCNA，這些都是我在在學期間，所得到的寶貴收穫，相信對我未來就業相當有幫助。
+                            </p>
+                        </div>
+                        <div class="single_resume" data-aos="fade-right">
+                            <span class="resume_duration">2006年9月-2009年7月</span>
+                            <h3>資訊科</h3>
+                            <span class="resume-designation">新民高級中學</span>
+                            <p>
+                                　我在小學電腦課第一次接觸到電腦，也開始對電腦感興趣，升上高中後，因此選擇了新民高中資訊科，在此時期，我開始接觸到網路世界，也墊下往後對網路服務的深厚興趣。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <h4 class="resume_title"><i class="fas fa-briefcase"></i>工作</h4>
+                        <div class="single_resume" data-aos="fade-left">
+                            <span class="resume_duration">2009年9月-2013年7月</span>
+                            <h3>直播小幫手</h3>
+                            <span class="resume-designation">全球藝品拍賣場</span>
+                            <p>
+                                我因為有各種相關知識所以有位置都可任職，我的工作包含了線上回覆訊息小編，處理客人訂單與售後服務，因為還有程式語言概念讓我自學EXCEL、Google
+                                sheet，編寫VBA(VisualBasic)與Google Apps
+                                Script(JavaScript)執行巨集，建立公司的電腦系統訂單架構，讓原本都是手寫的商品單據都改成線上管理，執行各項商品計算統整方面都比原本簡易許多，也同時教了公司員工使用我設計的程式，讓每個相關小幫手都有辦法快速的處理各項事務。
+                            </p>
+                        </div>
+                        <div class="single_resume" data-aos="fade-left">
+                            <span class="resume_duration">2009年9月-2013年7月</span>
+                            <h3>網拍人員</h3>
+                            <span class="resume-designation">水晶藝品店</span>
+                            <p>
+                                學習到了很多水晶藝品類的知識，也練習跟客人溝通各項藝品的差異、辨識與說明，並不是任何礦石開採後都會是保持完美狀態，並且利用在學時所用到的資訊網路與程式概念自行架設網頁，也在FB社團上進行行銷讓客人知道我們這家店。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="testimonial_area">
+            <div class="container">
+                <div class="row">
+                    <div class="title col-12" style="text-align: center;">
+                        <h1>見證者</h1>
+                        <p>XXXXXXXXXX<br>
+                            XXXXXXXXXX</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="contact_area">
+            <div class="container">
+                <div class="row">
+                    <div class="title col-12" style="text-align: center;">
+                        <h1>聯絡方式</h1>
+                        <p>XXXXXXXXXX<br>
+                            XXXXXXXXXX</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js">
+        <script>
 
-        function showPage() {
-            document.getElementById("loader").style.display = "none";
-            document.getElementById("myDiv").style.display = "block";
-            document.querySelector('body').style.overflowY = "scroll";
-        }
-        //
+            // 讀取畫面
+            var myVar;
 
-
-        let btn = document.querySelector('.btnbox');
-        let nav = document.querySelector('.navbar-nav');
-
-        btn.addEventListener('click', function () {
-            nav.classList.toggle('active');
-            btn.classList.toggle('active');
-        });
-
-        window.addEventListener('resize', function () {
-            nav.classList.remove('active');
-            btn.classList.remove('active');
-        });
-
-        //技能條
-        let progressBox = document.querySelector('.progressBox');
-        window.addEventListener('scroll', scrollProgressBarHandler);
-        function scrollProgressBarHandler() {
-            //設定螢幕最上邊的卷軸高度
-            let windowTop = window.scrollY;
-            //設定螢幕最下邊的卷軸高度
-            let windowBottom = windowTop + window.innerHeight;
-
-            let progressBoxMiddle = progressBox.offsetTop + parseInt(window.getComputedStyle(progressBox).height) / 2;
-
-            //當圖片中間卷軸高度小於螢幕底下卷軸高度時 且螢幕最上的高度比目前圖片底邊高度還小時顯示
-            if (progressBoxMiddle < windowBottom && progressBoxMiddle + parseInt(window.getComputedStyle(progressBox).height) / 2 > windowTop) {
-                progressBox.querySelectorAll('.progress .progress-bar').forEach(bar => {
-                    bar.classList.add('active');
-
-                });
-            } else {
-                progressBox.querySelectorAll('.progress .progress-bar').forEach(bar => {
-                    bar.classList.remove('active');
-                });
+            function opening() {
+                myVar = setTimeout(showPage, 3000);
             }
-        }
+
+            function showPage() {
+                document.getElementById("loader").style.display = "none";
+                document.getElementById("myDiv").style.display = "block";
+                document.querySelector('body').style.overflowY = "scroll";
+                AOS.init();
+            }
+            //
+
+
+            let btn = document.querySelector('.btnbox');
+            let nav = document.querySelector('.navbar-nav');
+
+            btn.addEventListener('click', function () {
+                nav.classList.toggle('active');
+                btn.classList.toggle('active');
+            });
+
+            window.addEventListener('resize', function () {
+                nav.classList.remove('active');
+                btn.classList.remove('active');
+            });
+
+            //技能條
+            let progressBox = document.querySelector('.progressBox');
+            window.addEventListener('scroll', scrollProgressBarHandler);
+            function scrollProgressBarHandler() {
+                AOS.init();
+                //設定螢幕最上邊的卷軸高度
+                let windowTop = window.scrollY;
+                //設定螢幕最下邊的卷軸高度
+                let windowBottom = windowTop + window.innerHeight;
+
+                let progressBoxMiddle = progressBox.offsetTop + parseInt(window.getComputedStyle(progressBox).height) / 2;
+
+                //當圖片中間卷軸高度小於螢幕底下卷軸高度時 且螢幕最上的高度比目前圖片底邊高度還小時顯示
+                if (progressBoxMiddle < windowBottom && progressBoxMiddle + parseInt(window.getComputedStyle(progressBox).height) / 2 > windowTop) {
+                    progressBox.querySelectorAll('.progress .progress-bar').forEach(bar => {
+                        bar.classList.add('active');
+                    });
+                } else {
+                    progressBox.querySelectorAll('.progress .progress-bar').forEach(bar => {
+                        bar.classList.remove('active');
+                    });
+                }
+            }
+            //
+
+            //作品選擇器
+            let portfolios = document.querySelectorAll('.portfolio_filter>ul>li');
+            let cards = document.querySelectorAll('#work_area .card');
+            let interval = true;
+            portfolios.forEach(portfolio => {
+                portfolio.addEventListener('click', function () {
+                    if (interval) {
+                        //半秒內只能執行一次
+                        interval = false;
+                        setTimeout(function () {
+                            interval = true;
+                        }, 500)
+
+                        //選擇到的類別
+                        //先清除所有類別
+                        portfoliosClear();
+                        this.classList.add('active')
+
+                        //顯示類別的卡片
+                        cards.forEach(card => {
+                            if (this.dataset.type == 'all') {
+                                card.classList.remove('none');
+                                setTimeout(function () {
+                                    card.classList.add('active');
+                                }, 100);
+
+                            } else {
+                                //清除所有卡片,除了被選擇到的類別卡片
+                                cardsClearEx(this.dataset.type);
+
+                                if (card.classList.contains(this.dataset.type)) {
+                                    card.classList.remove('none');
+                                    setTimeout(function () {
+                                        card.classList.add('active');
+                                    }, 100);
+                                }
+                            }
+                        })
+                    }
+                })
+            })
+            //清除所有類別
+            function portfoliosClear() {
+                portfolios.forEach(portfolio => {
+                    portfolio.classList.remove('active')
+                })
+            }
+            //清除所有卡片,除了被選擇到的類別卡片
+            function cardsClearEx(type) {
+                cards.forEach(card => {
+                    if (!card.classList.contains(type)) {
+                        card.classList.remove('active')
+                    }
+                })
+            }
+
+
+            let work_area = document.querySelector('#work_area');
+            work_area.addEventListener('transitionend', function (e) {
+                if (e.propertyName == 'opacity') {
+                    if (!e.target.classList.contains('active')) {
+                        e.target.classList.add('none');
+                    }
+                    // console.log(e.target.classList);
+
+                }
+            });
         //
-    </script>
+
+
+
+        </script>
 
 </body>
 
